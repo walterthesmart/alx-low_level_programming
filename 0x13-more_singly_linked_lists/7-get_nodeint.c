15304a20662d1fe26f929969fa3e0d860efdfd78
Walterthesmart <nwaugowalter@gmail.com>
2023-04-04 03:27:27 +0100

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,7 @@
  * get_nodeint_at_index - Returns the node at an index
  * @head: Pointer to the first element of the list
  * @index: Desired node at this position
- * Return: Pointer to the desired node
+ * Return: Pointer to the desired nodes
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
