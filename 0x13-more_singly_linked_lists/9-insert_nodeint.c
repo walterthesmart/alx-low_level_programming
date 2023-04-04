d6bcc9704d175b1f4e5c41ffbeb129db511d6ab6
Walterthesmart <nwaugowalter@gmail.com>
2023-04-04 03:28:23 +0100

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,6 +7,7 @@ listint_t *create_new_node(int n);
  * @n: number to be inserted into the node
  * Return: The address of the newly created node
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
